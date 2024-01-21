ae514ba104fc7cd312796d9a425f2098a11f265a
Wilson Franca de Souza <wilson.franca.92@gmail.com>
2024-01-20 17:55:26 -0800

    feat(docs): installed and configured storybook to work with next.js and tailwindCSS, keeping it with a dark theme

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      });
+    }
+
+    return config;
+  },
+};
+export default config;
